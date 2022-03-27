a1152d8552bd12e052b5a5486086ebd55b75bef4
Roshu00 <urosninkovic@yahoo.com>
2022-03-27 13:51:21 +0200

    Creating navigation bar

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const searchButton = document.querySelector(".search-icon");
+const searchContainer = document.querySelector(".search-container");
+const burgerMenu = document.querySelector(".burger");
+const smallNav = document.querySelector(".under-nav");
+
+const handleSearchOpen = () => {
+  searchContainer.classList.toggle("open");
+};
+
+const handleOpenMenu = () => {
+  smallNav.classList.toggle("open");
+};
+
+burgerMenu.addEventListener("click", handleOpenMenu);
+
+searchButton.addEventListener("click", handleSearchOpen);
