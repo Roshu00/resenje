aee9f9a123c9e6898948b89ee7680d8fe05d0589
Roshu00 <urosninkovic@yahoo.com>
2022-03-30 19:39:40 +0200

    slider controles added

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const searchButton = document.querySelector(".search-icon");
 const searchContainer = document.querySelector(".search-container");
 const burgerMenu = document.querySelector(".burger");
 const smallNav = document.querySelector(".under-nav");
+const slider = document.querySelector(".slider");
+const textSlider = document.querySelector(".text-slider");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
 
 const handleSearchOpen = () => {
   searchContainer.classList.toggle("open");
@@ -14,3 +18,32 @@ const handleOpenMenu = () => {
 burgerMenu.addEventListener("click", handleOpenMenu);
 
 searchButton.addEventListener("click", handleSearchOpen);
+
+var sectionIndex = 0;
+
+rightArrow.addEventListener("click", () => {
+  if (sectionIndex >= 2) {
+    return;
+  }
+  sectionIndex += 1;
+  slider.style.transform = "translate(" + sectionIndex * "-33.333333" + "%)";
+  textSlider.style.transform =
+    "translate(" + sectionIndex * "-33.333333" + "%)";
+});
+
+leftArrow.addEventListener("click", () => {
+  if (sectionIndex <= 0) {
+    return;
+  }
+  sectionIndex -= 1;
+  slider.style.transform = "translate(" + sectionIndex * "-33.333333" + "%)";
+  textSlider.style.transform =
+    "translate(" + sectionIndex * "-33.333333" + "%)";
+});
+
+const handleRadioChange = (value) => {
+  sectionIndex = value;
+  slider.style.transform = "translate(" + sectionIndex * "-33.333333" + "%)";
+  textSlider.style.transform =
+    "translate(" + sectionIndex * "-33.333333" + "%)";
+};
